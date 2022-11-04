53dc0ea504f947e5fc9ba4945cfec302437b297c
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2022-11-04 13:43:21 +0200

    buttons

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,44 @@
 export default function Home() {
   return (
-    <div>Monochrome <i className="fa-solid fa-house"></i></div>
+    <div className="text-secondary fs-3 fw-bold text-center">Monochrome
+      <div className="mt-5 text-center">
+
+        {/*mobile */}
+        <a className="button button-primary"
+          href=""
+          title="View all">
+          View all
+        </a>
+
+        {/* desktop */}
+        <a className="mx-2 button button-primary button-large"
+          href=""
+          title="View all">
+          View all
+        </a>
+
+        {/* footer */}
+        <button className="button button-primary button-secondary"
+          type="submit"
+          title="Submit">
+          Submit
+        </button>
+
+        {/* log in */}
+        <button className="ms-2 button button-primary button-inverted"
+          type="submit"
+          title="Sign in">
+          Sign in
+        </button>
+
+        <button className="ms-2 button button-primary button-inverted"
+          type="submit"
+          title="Register">
+          Register
+        </button>
+
+      </div>
+    </div >
+
   );
 }
