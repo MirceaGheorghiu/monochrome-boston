e5c201d03fec97ef5fcaa0b1771c711f619e0657
MirceaGheorghiu <110449045+MirceaGheorghiu@users.noreply.github.com>
2022-11-09 16:20:05 +0200

    header

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,44 +1,15 @@
+import Header from "../components/common/header/Header";
+
 export default function Home() {
   return (
-    <div className="text-secondary fs-3 fw-bold text-center">Monochrome
-      <div className="mt-5 text-center">
+    <div>
+      <header className="header">
+        <Header></Header>
+      </header>
 
-        {/*mobile */}
-        <a className="button button-primary"
-          href=""
-          title="New in">
-          New in
-        </a>
+      <main className="content"></main>
 
-        {/* desktop */}
-        <a className="mx-2 button button-large"
-          href=""
-          title="View all">
-          View all
-        </a>
-
-        {/* footer */}
-        <button className="button button-primary button-secondary"
-          type="submit"
-          title="Submit">
-          Submit
-        </button>
-
-        {/* log in */}
-        <button className="ms-2 button button-primary button-inverted"
-          type="submit"
-          title="Sign in">
-          Sign in
-        </button>
-
-        <button className="ms-2 button button-primary button-inverted"
-          type="submit"
-          title="Register">
-          Register
-        </button>
-
-      </div>
-    </div >
-
+      <footer className="footer"></footer>
+    </div>
   );
 }
